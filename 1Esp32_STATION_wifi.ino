0d06da97235ac2cfd97eae65cfdb3fdae04f96bf
Nandu126 <148153963+Nandu126@users.noreply.github.com>
2023-11-06 17:07:43 -0500

    Update 1Esp32_STATION_wifi.ino

--- 1Esp32_STATION_wifi.ino
+++ 1Esp32_STATION_wifi.ino
@@ -1,32 +1,10 @@
 #include <WiFi.h>                                     // needed to connect to setup an accesspoint
 #include <Wire.h>
-#include <Adafruit_MotorShield.h>
 
-
-// SSID and password that are going to be used for the Access Point you will create -> DONT use the SSID/Password of your router:
 const char* ssid = "ZhouLab";
 const char* password = "ZhouRobotics917";
 
 WiFiServer server(80);
-
-// Variable to store the HTTP request
-String header;
-//variables to store the current motor states
-String statemotor1 = "off";
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0;
-// Define timeout time in milliseconds
-const long timeoutTime = 2000;
-
-
-//Adafruit_MotorShield object
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-//DC motor object
-Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
-
 
 
 void setup() {
@@ -49,81 +27,5 @@ void setup() {
 }
 
 void loop() {
-   WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {
-      // loop while the client's connected
-      currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-
-            // turns the motor on and off
-            if (header.indexOf("GET /1/on") >= 0) {
-              statemotor1 = "on";
-              motor1->setSpeed(100);
-              motor1->run(FORWARD);               // turns the motor on
-            } else if (header.indexOf("GET /1/off") >= 0) {
-              statemotor1 = "off";
-              motor1->run(RELEASE);                //turns the motor off
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            client.println("<style>html { font-family: monospace; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: yellowgreen; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 32px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: gray;}</style></head>");
-
-            client.println("<body><h1>ESP32 Web Server</h1>");
-            client.println("<p>Control Motor State</p>");
-
-            if (statemotor1 == "off") {
-              client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-         
-            client.println("</body></html>");
-
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
 }
 
